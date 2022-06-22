634f94223b6f25e69b4c60680c688797193b921a
jdarlan.cm <jdarlan@gmail.com>
2022-06-21 23:29:37 -0300

    Commit diario

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,9 +1,43 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+import { query as q } from "faunadb";
 
+type User = {
+  ref: {
+    id: string;
+  }
+}
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if(req.method === 'POST') {
-    const checkoutSession = await stripe.checkout.sessions.create({
+    const session = await getSession({req});
+    
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(
+          q.Index('user_by_email'),
+          q.Casefold(session.user.email),
+        )
+      )
+    )
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email,
+    })
+
+    await fauna.query(
+      q.Update(
+        q.Ref(q.Collection('users'), user.ref.id),
+        {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          }
+        }
+      )
+    )
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
@@ -14,6 +48,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       success_url: 'http://localhost:300/posts',
       cancel_url: 'http://localhost:300'
     });
+
+    return res.status(200).json({sessionID: stripeCheckoutSession.id})
   } else {
     res.setHeader('Allow','POST');
     res.status(405).end('Method not allowed');
