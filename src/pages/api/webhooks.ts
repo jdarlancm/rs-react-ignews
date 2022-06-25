109c567a41ea573841e2aaeb228acd28241da8d3
jdarlan.cm <jdarlan@gmail.com>
2022-06-25 00:05:21 -0300

    Mudancas

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from 'stream';
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
   const chunks = [];
@@ -22,7 +23,10 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-  'checkout.session.completed'
+  'checkout.session.completed',
+  'customer.subscription.created',
+  'customer.subscription.update',
+  'customer.subscription.delte',
 ]);
 
 const webhooks = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -40,16 +44,32 @@ const webhooks = async (req: NextApiRequest, res: NextApiResponse) => {
     }
 
     const {type} = event;
-
+    
     try {
       if(relevantEvents.has(type)) {
         switch(type) {
+          case 'customer.subscription.created':
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            console.log('HERE!!!!');
+            const subscription = event.data.object as Stripe.Subscription;
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              type === 'customer.subscription.created',
+            );
+            break;
           case 'checkout.session.completed':
+            const checkoutSession = event.data.object as Stripe.Checkout.Session;
+            await saveSubscription(
+              checkoutSession.subscription.toString(),
+              checkoutSession.customer.toString(),
+              true,
+            );
             break;
           default:
             throw new Error('Unhandled event.');
         }
-        console.log('evento recebido', event);
       }
     } catch (err) {
       //sentry, bugsnag
